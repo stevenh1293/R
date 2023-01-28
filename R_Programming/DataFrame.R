a7aa5020db996b3bf22db14bbed7920c4862af27
stevenh1293 <72676164+stevenh1293@users.noreply.github.com>
2023-01-28 15:24:10 -0500

    Update DataFrame.R

--- R_Programming/DataFrame.R
+++ R_Programming/DataFrame.R
@@ -1,9 +1,8 @@
 Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
-
 ABC <- c(4, 62, 51, 21, 2, 14, 15)
-
 CBS <- c(12, 75, 43, 19, 1, 21, 19) 
 
 Poll.df <- data.frame(Name,ABC,CBS)
 
+str(Poll.df)
 Poll.df
