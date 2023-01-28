4570074ab79ec7920d807fe84195b923a43561b5
Steven Horton <stevenh1293@gmail.com>
2023-01-28 14:49:08 -0500

    Assignment 2

---
+++ R_Programming/DataFrame.R
@@ -0,0 +1,9 @@
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS <- c(12, 75, 43, 19, 1, 21, 19) 
+
+Poll.df <- data.frame(Name,ABC,CBS)
+
+Poll.df
---
+++ R_Programming/MyMean.R
@@ -0,0 +1,13 @@
+#Assignment example
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+
+myMean(assignment2)
+
+
+#My example
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(arg) { return(sum(arg)/length(arg)) }
+
+myMean(assignment2)
+
