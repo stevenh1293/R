229b8d6f2f3768d4952ff2c9e9902905e9387c99
Steven Horton <stevenh1293@gmail.com>
2023-03-27 01:56:00 -0400

    Create Debugging.R

---
+++ R_Programming/Debugging.R
@@ -0,0 +1,27 @@
+#Bugged function
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE,dim=dim(x))
+  for (j in 1:ncol(x))
+  {
+    outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+  }
+  outlier.vec <- vector(length=nrow(x))
+  for (i in 1:nrow(x))
+  { outlier.vec[i] <- all(outliers[i,]) } return(outlier.vec) }
+
+ traceback()
+ 
+ #Debugged function
+ tukey_multiple <- function(x) {
+   outliers <- array(TRUE,dim=dim(x))
+   for (j in 1:ncol(x))
+   {
+     outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
+   }
+   outlier.vec <- vector(length=nrow(x))
+   for (i in 1:nrow(x))
+   { outlier.vec[i] <- all(outliers[i,]) 
+   return(outlier.vec) }
+ }
+
+  
